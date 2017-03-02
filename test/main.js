1fabd20f5fefe277f19e221d15740d48577a744a
I'amee <admin@cavacn.com>
2017-03-02 19:42:53 +0800

    这个版本暂时通过~

--- test/main.js
+++ test/main.js
@@ -1,10 +1,24 @@
 /**
  * Created by Z on 2017-03-01.
  */
+'use strict'
 const Api = require('..');
 const path = require('path');
 const api = new Api();
+const fs = require('fs');
+const template = fs.readFileSync(path.join(__dirname, 'template', 'api.ejs')).toString();
+api.load(path.join(__dirname, './apis/api.raml'));
 
-api.load(path.join(__dirname,'./apis/api.raml'));
+// api.removeController(path.join(__dirname,'controllers'));
 
-console.log(api.routers)
+// api.developController(template);
+
+api.invoke('GET /home/user/1000/eat', {aaa: 'aaaa', id: '01', uname: 'iamee'}, (err, result, code) => {
+    console.log(err, result, code)
+    if (err) {
+        console.error(err);
+    }
+    if (result) {
+        console.log(result);
+    }
+})
