5f266465db04a2f46bd71fa0e7829b892ca80567
我是EE <admin@cavacn.com>
2017-03-13 21:01:06 +0800

    修复baseUri 问题

--- test/controllers/home.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Z on 2017-03-06.
- */
-
-const ApiError = require('../../lib').ApiError;
-
-module.exports.get = function(){
-    throw new ApiError('错误，各种错误',303);
-}
---
+++ test/controllers/home/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by iZhui on 2017/3/13.
+ */
+module.exports.get = async function(){
+    return { id:"01", uname:'xxxx'}
+}
