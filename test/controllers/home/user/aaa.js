25aac2e8b9ba15dd42336b239a95a541200c6950
I'amee <admin@cavacn.com>
2017-03-15 10:56:57 +0800

    修改cache使用依赖模块cds-hot

--- test/controllers/home/user/aaa.js
+++ test/controllers/home/user/aaa.js
@@ -1,6 +1,9 @@
 /**
  * Created by iZhui on 2017/3/13.
  */
+
+const cdsRequest = require('cds-hot').cdsRequire
+
 module.exports.get = async function(){
-    return 'aaaccc----xxccce'
+    return cdsRequest('./logic/demo');
 }
---
+++ test/logic/demo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Z on 2017-03-15.
+ */
+module.exports = [
+    'GGxxx'+Date.now()
+]
